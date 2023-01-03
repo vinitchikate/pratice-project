3a0b3219c1c9aebeb0ca7d774f5c4ba439a9deac
vinitchikate <102968505+vinitchikate@users.noreply.github.com>
2023-01-03 20:08:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //index. js typically handles your app startup, 
 //routing and other functions of your application and does require other modules to add functionality. 
-//If you're running a website or web app it would also handle become a basic HTTP web server replacing the role of
+//If you're running a website or web app it would also handle because a basic HTTP web server replacing the role of
 // something more traditional like Apache.
 const express = require('express');
 const bodyParser = require('body-parser');
